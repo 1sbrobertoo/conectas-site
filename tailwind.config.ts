43344f289a65e5571c592eec93260574a262540d
Roberto Júnior <104289197+1sbrobertoo@users.noreply.github.com>
2026-04-17 05:02:51 -0300

    Create tailwind.config.ts

---
+++ tailwind.config.ts
@@ -0,0 +1,19 @@
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'conectas-blue': '#194B64',
+        'conectas-gold': '#E9CD6A',
+      },
+    },
+  },
+  plugins: [],
+}
+export default config
